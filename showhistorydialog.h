20189d112e0fe7e4c15c07a8105e2251d2c8796a
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:13:59 +0800

    Update showhistorydialog.h

--- showhistorydialog.h
+++ showhistorydialog.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #pragma once
@@ -31,4 +31,5 @@ private:
 	QVector<QString> m_classNames;
 	inline static const QString allClassHolder{"..."};
 };
+
 
