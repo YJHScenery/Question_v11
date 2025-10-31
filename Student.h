aaff5097fbc850f479ad54347a3430fd91ab9c5d
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:09:55 +0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #pragma once
 
@@ -111,3 +111,4 @@ namespace QK {
     QDataStream& operator<<(QDataStream& out, const Student& s);
     QDataStream& operator>>(QDataStream& in, Student& s);
 }
+
