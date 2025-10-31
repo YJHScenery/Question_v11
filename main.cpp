2e8795954a423686af1b1729a68ac68b00cf3192
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:11:41 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #include "global.h"
 #include <QApplication>
@@ -28,3 +28,4 @@ int main(int argc, char *argv[]) {
     window->show();
     return app.exec(); 
 }
+
