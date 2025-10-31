aea4f38988bff31afe4cf3dae1071ec410ac7976
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:07:57 +0800

    Update ClassDashboard.h

--- ClassDashboard.h
+++ ClassDashboard.h
@@ -1,5 +1,5 @@
 #pragma once
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #include "global.h"
@@ -55,3 +55,4 @@ namespace QK {
     };
 
 } // namespace QK
+
