52dff6d9dfb1029ee264748c710c7e02b4bfecff
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:12:11 +0800

    Update newclassdialog.cpp

--- newclassdialog.cpp
+++ newclassdialog.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #include "global.h"
@@ -248,3 +248,4 @@ void NewClassDialog::initSet() const
 	ui.visibleButton->setIcon(invisible_icon);
 	ui.visibleButton->setIconSize(ui.visibleButton->size());
 }
+
