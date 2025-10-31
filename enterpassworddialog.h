ccacd773312c3f0fcbb21d6ce7f81585cd9971cc
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:11:24 +0800

    Update enterpassworddialog.h

--- enterpassworddialog.h
+++ enterpassworddialog.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #pragma once
 
@@ -21,4 +21,5 @@ protected slots:
 private:
 	Ui::EnterPasswordDialogClass ui;
 };
+
 
