dc8bfc18db24e4154786d24c11deeb6544e3c4e6
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:09:07 +0800

    Update QuestionHistory.cpp

--- QuestionHistory.cpp
+++ QuestionHistory.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #include "global.h"
@@ -247,4 +247,5 @@ namespace QK {
     return XlsxIEState::ExportFileError;
 #endif
     }
+
 } // namespace QK
