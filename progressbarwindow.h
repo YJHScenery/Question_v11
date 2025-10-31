f5f7f0c97e9b9c6b04943ae74d9c33b2af5c038a
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:12:52 +0800

    Update progressbarwindow.h

--- progressbarwindow.h
+++ progressbarwindow.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #pragma once
@@ -32,4 +32,5 @@ private:
     QElapsedTimer elapsedTimer; // 计时器
 	Ui::ProgressBarWindowClass *ui;
 };
+
 
