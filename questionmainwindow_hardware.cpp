b12776f2c42648e134a084b1a4e340d4d4f57110
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:13:38 +0800

    Update questionmainwindow_hardware.cpp

--- questionmainwindow_hardware.cpp
+++ questionmainwindow_hardware.cpp
@@ -1,3 +1,5 @@
+// Copyright (C) 2025 玛卡巴卡YJH
+// SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #include "global.h"
 
 #include "questionmainwindow.h"
@@ -157,5 +159,6 @@ void QuestionMainWindow::onButtonResetClicked_H() const
 {
     ui->countEditor->clear();
 }
+
 
 
