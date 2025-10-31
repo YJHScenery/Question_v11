eedc8a3e9068086a292ef4f1957626f260989a29
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:08:37 +0800

    Update Question.h

--- Question.h
+++ Question.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #pragma once
 
@@ -172,3 +172,4 @@ namespace QK {
 #endif
     };
 }
+
