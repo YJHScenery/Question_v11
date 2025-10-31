fe3f8aa8e5224f47c587c1f759bb209226c86bd6
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:07:44 +0800

    Update copyright and include statements in ClassDashboard

--- ClassDashboard.cpp
+++ ClassDashboard.cpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 #include "global.h"
 #include "ClassDashboard.h"
@@ -236,5 +236,6 @@ namespace QK {
         }
         return data;
     }
+
 
 } // namespace QK
