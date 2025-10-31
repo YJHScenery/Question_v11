289cce41f863a5825b2a80b86e29a56b39053fe0
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:12:28 +0800

    Update pathprocess.hpp

--- pathprocess.hpp
+++ pathprocess.hpp
@@ -1,5 +1,5 @@
 #pragma once
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 #include "global.h"
@@ -27,3 +27,4 @@ inline QString processDataPath(const QVector<QString>& args) {
     }
     return DEFAULT_DATA_DIRECTORY;
 }
+
