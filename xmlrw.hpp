5483f0a166ac4763626343a6e2e512d0f33129b3
老叶 <89681507+YJHScenery@users.noreply.github.com>
2025-10-31 09:14:08 +0800

    Update copyright and comments in xmlrw.hpp

--- xmlrw.hpp
+++ xmlrw.hpp
@@ -1,4 +1,4 @@
-// Copyright (C) 2025 叶景皓 Ye Jinghao
+// Copyright (C) 2025 玛卡巴卡YJH
 // SPDX-License-Identifier: GPL-3.0-only OR LGPL-3.0-only
 
 /*
@@ -107,4 +107,5 @@ inline bool loadWindowConfig(const QString& filePath, WindowConfig& config) {
         config.m_animateChecked = static_cast<bool>(animateCheckedElem.text().toInt());
 
     return true;
+
 }
